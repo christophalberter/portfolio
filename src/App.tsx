8a198d906f7e9b2ada07f8ea26ee114a2fcdf520
DESKTOP-hcsilva <m.henriquesilva18@gmail.com>
2023-04-09 22:02:47 +0200

    refactor: folder struture

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css';
-import React from 'react';
+import './App.css'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Projects from './pages/Projects'
 
 export default function App() {
-  return (
-    <>
-      <h1>This is a portefolio</h1>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
-
