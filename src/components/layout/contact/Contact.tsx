6c0e91838a4b9b1eb0ccd25f38b43794fab0af61
Mauro <m.henriquesilva18@gmail.com>
2024-04-01 16:42:55 +0200

    .

--- src/components/layout/contact/Contact.tsx
+++ src/components/layout/contact/Contact.tsx
@@ -6,12 +6,9 @@ export const Contact = () => {
       <form method="POST" action="https://getform.io/f/paygpgya" className="flex flex-col max-w-[600px] w-full">
         <div className="py-10">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
-          <p className="text-gray-300 py-3">Submit the form below or you can directly contact me.</p>
+          <p className="text-gray-300 py-3">Send me a message 😉 </p>
 
           <p />
-          <a href="tel:+41794213680" className="text-gray-300  hover:text-white">
-            +41 794213680
-          </a>
         </div>
         <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name="name" required />
         <input className="my-4 p-2 bg-[#ccd6f6]" type="email" placeholder="Email" name="email" required />
